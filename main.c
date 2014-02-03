8b20a0cbdb4272aa824fcadc398d88b50b06b784
xurui203 <xurui203@gmail.com>
2014-02-02 20:21:43 -0500

    Untested server code

--- main.c
+++ main.c
@@ -12,19 +12,22 @@ int client(const char * addr, uint16_t port);
 #define MAX_MSG_LENGTH (1300)
 #define MAX_BACK_LOG (5)
 
+//argc = argument count = number of strings pointed to by argv
+//argv = argument vector
 int main(int argc, char ** argv)
 {
-	if (argc < 3) {
+	if (argc < 3) { 
 		printf("usage: myprog c <port> <address> or myprog s <port>\n");
 		return 0;
 	}
+
 
 	uint16_t port = atoi(argv[2]);
 	if (port < 1024) {
 		fprintf(stderr, "port number should be equal to or larger than 1024\n");
 		return 0;
 	}
-	if (argv[1][0] == 'c') {
+	if (argv[1][0] == 'c') { 
 		return client(argv[3], port);
 	} else if (argv[1][0] == 's') {
 		return server(port);
@@ -35,22 +38,27 @@ int main(int argc, char ** argv)
 	return 0;
 }
 
-int client(const char * addr, uint16_t port)
+int client(const char * addr, uint16_t port) //server's IP address, server's port number
 {
 	int sock;
 	struct sockaddr_in server_addr;
 	char msg[MAX_MSG_LENGTH], reply[MAX_MSG_LENGTH];
 
+	//Create socket
 	if ((sock = socket(AF_INET, SOCK_STREAM/* use tcp */, 0)) < 0) {
 		perror("Create socket error:");
 		return 1;
 	}
 
 	printf("Socket created\n");
-	server_addr.sin_addr.s_addr = inet_addr(addr);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
 
+	//Construct the address data structure expected by socket interface
+	server_addr.sin_addr.s_addr = inet_addr(addr); //set address to be address given
+	server_addr.sin_family = AF_INET; //set protocol family to be Internet TCP
+	server_addr.sin_port = htons(port); //specify server port to be port number given in argument
+
+
+	// Active open, connect to server
 	if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
 		perror("Connect error:");
 		return 1;
@@ -79,8 +87,48 @@ int client(const char * addr, uint16_t port)
 
 int server(uint16_t port)
 {
-	/*
-		Add your code here
-	*/
+	int sock, new_sock;;
+	struct sockaddr_in client_addr;
+	char buf[MAX_MSG_LENGTH];
+	int recv_len;
+
+	//Construct the address data structure by filling in its own port number
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_addr = INADDR_ANY;
+	client_addr.sin_port = htons(port);
+
+
+	//Passive open: Create socket
+	
+	if ((sock = socket(AF_INET, SOCK_STREAM/, 0)) < 0) {
+		perror("Create socket error:");
+		return 1;
+	}
+
+
+	//Bind socket to local address
+
+	if ((bind(sock, (struct sockaddr *)&client_addr, sizeof(client_addr)) < 0){
+		perror("Binding error:");
+		return 1;
+	}
+
+	//Set up maximum number of pending connections to be allowed
+	listen(sock, MAX_BACK_LOG);
+
+	//Loop: wait for a remote host to try to connect.
+		//When connected, receive and echo characters that arrive on the connection
+
+	while (1){
+		if ((new_sock = accept(sock, (struct sockaddr *)&client_addr, &recv_len)) < 0){ //&??
+			perror("Accepting error:");
+			return 1;
+		}
+
+		while (recv_len = recv(new_sock, buf, sizeof(buf), 0)){
+			fputs(buf, stdout);
+		}
+		close(new_sock);
+	}
 	return 0;
 }
